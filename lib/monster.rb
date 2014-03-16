bf571172246e873e9e6aba8cea054616c3d91772
Kyle Daigle <kyle.daigle@github.com>
2014-03-15 22:24:53 -0400

    Calculate health points

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,11 +1,22 @@
 class Monster
 
-  attr_reader :name, :level, :difficulty
+  HEALTH_POINTS_FACTOR = 3.freeze
 
-  def initialize(name, level, difficulty)
+  attr_reader :name, :level, :difficulty, :strength, :dexterity, :constitution, :intelligence
+
+  def initialize(name, level, difficulty, attributes = {})
     @name = name
     @level = level
     @difficulty = difficulty
+
+    @strength     = attributes[:strength] || 0
+    @dexterity    = attributes[:dexterity] || 0
+    @constitution = attributes[:constitution] || 0
+    @intelligence = attributes[:intelligence] || 0
+  end
+
+  def health_points
+    constitution * HEALTH_POINTS_FACTOR
   end
 
 end
--- spec/lib/monster_spec.rb
+++ spec/lib/monster_spec.rb
@@ -2,7 +2,7 @@ require "monster"
 
 describe Monster do
 
-  subject { Monster.new("Skeleton King", 5, 50) }
+  subject { Monster.new("Skeleton King", 5, 50, {strength: 5, dexterity: 10, constitution: 15, intelligence: 3}) }
 
   context "#initialize" do
     it "sets the name" do
@@ -15,6 +15,19 @@ describe Monster do
 
     it "sets the difficulty" do
       expect(subject.difficulty).to eql(50)
+    end
+
+    it "sets the attributes" do
+      expect(subject.strength).to eql(5)
+      expect(subject.dexterity).to eql(10)
+      expect(subject.constitution).to eql(15)
+      expect(subject.intelligence).to eql(3)
+    end
+  end
+
+  context "#health_points" do
+    it "calculates health points based on constitution" do
+      expect(subject.health_points).to eql(45)
     end
   end
 end
